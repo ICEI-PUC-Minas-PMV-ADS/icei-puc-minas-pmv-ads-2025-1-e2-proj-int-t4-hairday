085c92621aa79325349e965ada845030a16d3835
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-05-23 12:05:45 -0300

    Primeira parte da pagina de agendar horarios

---
+++ src/Hairday/Controllers/AgendarHorarioController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hairday.Models;
+using System;
+using System.Linq;
+using Hairday.Controllers;
+
+public class AgendarHorarioController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public AgendarHorarioController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Selecionar()
+    {
+        ViewBag.ProximosDias = Enumerable.Range(0, 10)
+            .Select(i => DateTime.Today.AddDays(i))
+            .ToList();
+
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult HorariosDisponiveis(string data)
+    {
+        if (!DateTime.TryParse(data, out DateTime dataSelecionada))
+        {
+            return BadRequest("Data inválida");
+        }
+
+        string cpfBarbeiro = "12345678901";
+
+        var horariosDisponiveis = _context.Horario_Disponivel
+            .Where(h => h.CPF_barbeiro == cpfBarbeiro && h.data == dataSelecionada && h.ocupado == false)
+            .Select(h => h.hora)
+            .ToList();
+
+        return PartialView("_HorariosDisponiveis", horariosDisponiveis);
+    }
+}
+
