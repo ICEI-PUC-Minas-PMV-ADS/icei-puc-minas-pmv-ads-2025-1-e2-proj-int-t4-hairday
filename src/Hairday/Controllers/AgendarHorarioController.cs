b904212e5c4f9142d22f0f0ad6d767e9c926a3a2
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-05-26 12:42:01 -0300

    Adicionada função de exibir detalhes dos horários

--- src/Hairday/Controllers/AgendarHorarioController.cs
+++ src/Hairday/Controllers/AgendarHorarioController.cs
@@ -1,43 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Hairday.Models;
-using System;
-using System.Linq;
-using Hairday.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
-public class AgendarHorarioController : Controller
+
+//cpfBarbeiro = "12345678901"
+//id_servico == 1
+//int idCliente = 1;
+
+
+namespace Hairday.Controllers
 {
-    private readonly AppDbContext _context;
-
-    public AgendarHorarioController(AppDbContext context)
+    public class AgendarHorarioController : Controller
     {
-        _context = context;
-    }
+        private readonly AppDbContext _context;
 
-    public IActionResult Selecionar()
-    {
-        ViewBag.ProximosDias = Enumerable.Range(0, 10)
-            .Select(i => DateTime.Today.AddDays(i))
-            .ToList();
-
-        return View();
-    }
-
-    [HttpGet]
-    public IActionResult HorariosDisponiveis(string data)
-    {
-        if (!DateTime.TryParse(data, out DateTime dataSelecionada))
+        public AgendarHorarioController(AppDbContext context)
         {
-            return BadRequest("Data inválida");
+            _context = context;
         }
 
-        string cpfBarbeiro = "12345678901";
+        public IActionResult Selecionar()
+        {
+            var datas = Enumerable.Range(0, 10)
+                .Select(i => DateTime.Today.AddDays(i))
+                .ToList();
 
-        var horariosDisponiveis = _context.Horario_Disponivel
-            .Where(h => h.CPF_barbeiro == cpfBarbeiro && h.data == dataSelecionada && h.ocupado == false)
-            .Select(h => h.hora)
-            .ToList();
+            ViewBag.Datas = datas;
 
-        return PartialView("_HorariosDisponiveis", horariosDisponiveis);
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> HorariosPorData(DateTime data)
+        {
+            var cpfBarbeiro = "12345678901";
+
+            var horarios = await _context.Horario_Disponivel
+                .Where(h => h.CPF_barbeiro == cpfBarbeiro && h.data == data && !h.ocupado)
+                .OrderBy(h => h.hora)
+                .ToListAsync();
+
+            return PartialView("_HorariosDisponiveis", horarios);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DadosResumo(DateTime data, string hora)
+        {
+            var cpfBarbeiro = "12345678901";
+
+            var barbeiro = await _context.Barbeiros
+                .FirstOrDefaultAsync(b => b.CPF_barbeiro == cpfBarbeiro);
+
+            var horario = TimeSpan.ParseExact(hora, @"hh\:mm", CultureInfo.InvariantCulture);
+
+            var servico = await _context.Servicos
+                .FirstOrDefaultAsync(s => s.id_servico == 1);
+
+            if (barbeiro == null || servico == null)
+            {
+                return NotFound();
+            }
+
+            var resultado = new
+            {
+                nomeBarbeiro = barbeiro.nome,
+                nomeServico = servico.nome_servico,
+                preco = servico.preco,
+                data = data.ToString("dd/MM/yyyy"),
+                hora = horario.ToString(@"hh\:mm")
+            };
+
+            return Json(resultado);
+        }
+
+       
+
     }
 }
-
