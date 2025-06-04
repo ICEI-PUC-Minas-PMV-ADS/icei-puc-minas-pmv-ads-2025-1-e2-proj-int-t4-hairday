947b7f5f5dd8544277051af313e047f03d652023
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-06-04 13:53:58 -0300

    Criada a pagina de exibir agendamentos do cliente, e antes de estilizar a pagina de agendar horarios

---
+++ src/Hairday/Controllers/AgendamentosClienteController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hairday.Controllers
+{
+    public class AgendamentosClienteController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AgendamentosClienteController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> MeusAgendamentos()
+        {
+            // Recupera o ID do cliente da sessão
+            var idCliente = HttpContext.Session.GetInt32("id");
+            if (idCliente == null)
+                return RedirectToAction("Login", "Account");
+
+            var agendamentos = await _context.Agendamentos
+                .Include(a => a.barbeiro)
+                    .ThenInclude(b => b.barbearia)
+                .Include(a => a.agendamento_servicos)
+                    .ThenInclude(asv => asv.servico)
+                .Where(a => a.id_cliente == idCliente)
+                .ToListAsync();
+
+            return View(agendamentos);
+        }
+    }
+}
