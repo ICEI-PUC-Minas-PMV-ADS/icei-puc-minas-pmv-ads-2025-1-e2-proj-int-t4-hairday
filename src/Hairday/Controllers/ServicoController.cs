910fc87819ee6f4d41f86de82e9f22d769c53b62
Vitor Bisi Vieira <vitor.bisi@sga.pucminas.br>
2025-06-07 15:38:02 -0300

    Modificações da página de agendar horário e adicionada página com meus agendamentos

--- src/Hairday/Controllers/ServicoController.cs
+++ src/Hairday/Controllers/ServicoController.cs
@@ -68,7 +68,7 @@ namespace Hairday.Controllers
             _context.Servicos.AddRange(servicosParaSalvar);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Index", "Servico");
+            return RedirectToAction("Index", "Home");
         }
 
         public async Task<IActionResult> Index()
